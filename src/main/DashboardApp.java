7c1351e0e6ae3600e27152c87f079ef8907498b3
Eric <spb828@naver.com>
2018-01-14 12:56:32 +0900

    Update TestCase, Add initSpeechToText

--- src/main/DashboardApp.java
+++ src/main/DashboardApp.java
@@ -5,24 +5,48 @@ import java.io.*;
 import com.ibm.watson.developer_cloud.http.HttpMediaType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;;
 
 public class DashboardApp {
-	public static void main(String[] args) {
-		System.out.println("Start DashboardApp");
+	private static final int numberOfTestCases = 1;
+	private static TestCase[] testCases;
+	
+	private static SpeechToText service = new SpeechToText();
+	private static RecognizeOptions options;
+	
+	private static void initTestCases() {
+		testCases = new TestCase[numberOfTestCases];
 		
-		SpeechToText service = new SpeechToText();
+		testCases[0] = new TestCase();
+		testCases[0].resourcePath = "resources/SpeechSample.wav";
+		testCases[0].transcript = "several tornadoes touch down as a line of severe1 thunderstorms swept through Colorado on Sunday";
+	}
+	
+	private static void initSpeechToText() {
 		service.setUsernameAndPassword(Credentials.UserName, Credentials.Password);
 
-		File audio = new File("resources/SpeechSample.wav");
-
-		RecognizeOptions options = new RecognizeOptions.Builder()
-		    .contentType(HttpMediaType.AUDIO_WAV)
-		    .build();
-
-		SpeechResults transcript = service.recognize(audio, options).execute();
-		System.out.println(transcript);
-		
-		System.out.println("End of DashboardApp");
+		options = new RecognizeOptions.Builder()
+				.timestamps(true)
+			    .contentType(HttpMediaType.AUDIO_WAV)
+			    .build();
+	}
+	
+	public static void main(String[] args) {
+		initTestCases();
+		initSpeechToText();
+ 
+		// Test
+		for (TestCase t: testCases) {
+			File audio = new File(t.resourcePath);
+			
+			t.result = service.recognize(audio, options).execute();
+			if (t.isCorrect())
+				System.out.println(t.isCorrect());
+			else 
+				System.out.println(t.findIndexOfWord());
+			
+			//System.out.println(t.result.getResults().get(0).getAlternatives().get(0).getTranscript());
+			
+			//System.out.println(t.result.getResults().get(0).getAlternatives().get(0).getTimestamps());
+		}
 	}
 }
--- src/main/TestCase.java
+++ src/main/TestCase.java
@@ -1,6 +1,52 @@
 package main;
 
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;;
+
 public class TestCase {
 	public String resourcePath = "";
 	public String transcript = "";
+	public SpeechResults result;
+	
+	private String resultTranscript() {
+		return this.result.getResults().get(0).getAlternatives().get(0).getTranscript();
+	}
+	
+	
+	public boolean isCorrect() {
+		String resultTranscript = this.resultTranscript();
+		resultTranscript = trimRight(resultTranscript);
+		
+		System.out.println(resultTranscript);
+		System.out.println(transcript);
+		
+		return resultTranscript.equals(transcript);
+	}
+	
+	public int findIndexOfWord() {
+		String[] resultArr, transcriptArr;
+		resultArr = this.resultTranscript().split(" ");
+		transcriptArr = this.transcript.split(" ");
+		
+		for (int i = 0; i < resultArr.length; i++) {
+			if (!(resultArr[i].equals(transcriptArr[i])))
+				return i;
+		}
+		return -1;
+	}
+	
+    public static String trimRight(String s) {
+        int i = s.length()-1;
+        while (i >= 0 && Character.isWhitespace(s.charAt(i))) {
+            i--;
+        }
+        return s.substring(0,i+1);
+    }
+    
+    public static String trimLeft(String s) {
+        int i = 0;
+        while (i < s.length() && Character.isWhitespace(s.charAt(i))) {
+            i++;
+        }
+        return s.substring(i);
+    }
 }
