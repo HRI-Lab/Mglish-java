d690a53eb10f5a29de59fab332ddbea1c6bde076
Eric <spb828@naver.com>
2018-01-24 18:23:25 +0900

    Get rid of Credential error

---
+++ src/main/Credentials.java
@@ -0,0 +1,6 @@
+package main;
+
+public class Credentials {
+	final static public String UserName = "ea1db480-2b77-4211-a899-342d2f5c2eb2";
+	final static public String Password = "z24fVkCycPsw";
+}
